9baa3b21c067be268149b42d8ba500a7f133bc46
Soniceso <732129014zby@gmail.com>
2023-04-05 20:43:57 +0800

    Update modules.py

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -7,7 +7,7 @@ import math
 
 import torch
 import torch.nn as nn
-
+from ultralytics.nn.pcov import Partial_conv3
 from ultralytics.yolo.utils.tal import dist2bbox, make_anchors
 
 
@@ -120,12 +120,11 @@ class TransformerBlock(nn.Module):
 
 class Bottleneck(nn.Module):
     # Standard bottleneck
-    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):  # ch_in, ch_out, shortcut, groups, kernels, expand
+    def __init__(self, c1, shortcut=True):  # ch_in, ch_out, shortcut, groups, kernels, expand
         super().__init__()
-        c_ = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, c_, k[0], 1)
-        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
-        self.add = shortcut and c1 == c2
+        self.cv1 = Partial_conv3(c1, 4, 'split_cat')
+        self.cv2 = Partial_conv3(c1, 4, 'split_cat')
+        self.add = shortcut
 
     def forward(self, x):
         return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
@@ -186,7 +185,7 @@ class C2f(nn.Module):
         self.c = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, 2 * self.c, 1, 1)
         self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
-        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+        self.m = nn.ModuleList(Bottleneck(self.c, shortcut) for _ in range(n))
 
     def forward(self, x):
         y = list(self.cv1(x).chunk(2, 1))
