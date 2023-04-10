3daf244b18ebba44589065ab0843cbde3c236c8b
Soniceso <732129014zby@gmail.com>
2023-04-10 10:25:09 +0800

    Update modules.py

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -29,9 +29,17 @@ class Conv(nn.Module):
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+        self.dcnv3 = DCNv3(c2, kernel_size=3, dilation=d)
+        self.ln=nn.BatchNorm2d(c2)
 
     def forward(self, x):
-        return self.act(self.bn(self.conv(x)))
+        x=self.act(self.bn(self.conv(x)))
+        x = x.permute(0, 2, 3, 1)
+        x = self.dcnv3(x)
+        x = x.permute(0, 3, 1, 2)
+        x = self.ln(x)
+        x = self.act(x)
+        return x
 
     def forward_fuse(self, x):
         return self.act(self.conv(x))
