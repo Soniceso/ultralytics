535b802df7721ce476efd2ad5a2e3e4319f7bac4
Bingyang Zhai <732129014zby@gmail.com>
2023-05-15 08:31:15 +0800

    Update modules.py

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -120,7 +120,7 @@ class TransformerBlock(nn.Module):
 
 class Bottleneck(nn.Module):
     # Standard bottleneck
-    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):  # ch_in, ch_out, shortcut, groups, kernels, expand
+    def __init__(self, c1, c2, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, shortcut, groups, kernels, expand
         super(Bottleneck, self).__init__()
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
